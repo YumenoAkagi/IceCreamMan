43dabda03304b797da88c894cbef1a3036e6899f
YumenoAkagi <68376091+YumenoAkagi@users.noreply.github.com>
2021-12-08 15:27:12 +0700

    player attack animation

---
+++ Assets/Scripts/Player/PlayerMeleeCombat.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMeleeCombat : MonoBehaviour
+{
+    Animator animator;
+
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Z))
+        {
+            EnemyMeleeAttack();
+        }
+    }
+
+    private void EnemyMeleeAttack()
+    {
+        animator.SetTrigger("melee");
+    }
+}
