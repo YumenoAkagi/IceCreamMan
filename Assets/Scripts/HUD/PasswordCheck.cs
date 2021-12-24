0b12a92f8b84c781dda54995e94f24c6743a862f
YumenoAkagi <68376091+YumenoAkagi@users.noreply.github.com>
2021-12-24 21:04:35 +0700

    password checker logic fix

--- Assets/Scripts/HUD/PasswordCheck.cs
+++ Assets/Scripts/HUD/PasswordCheck.cs
@@ -7,66 +7,29 @@ using UnityEngine.SceneManagement;
 
 public class PasswordCheck : MonoBehaviour
 {
-    private string UnderTunnel = "dark";
-    private string RefrigeratorRoom = "fake";
-    private string DiningRoom = "cook";
-    private string WorkspaceRoom = "work";
-    private string WarehouseRoom = "ware";
-    private string RightTunnel = "tunnel";
-    private string LeftTunnel = "tunnel";
-    private string WarehouseRoom2 = "house";
+    public GameObject Door;
+    public string DoorPassword;
+    public InputField UserInput;
 
-    public InputField UserInput, UserInput2;
-    public string PasswordUserInput;
+    public GameObject CorrectPanel, IncorrectPanel;
 
-    private string Room;
-    private string correct = "Scenes/correct";
-    private string failed = "Scenes/failed";
-
-    private string UTRoom = "UndergroundTunnel";
-    private string FKRoom = "FakeRefrigerator";
-    private string DRoom = "DiningRoom";
-    private string WorkRoom = "WorkspaceRoom";
-    private string WareRoom = "WarehouseRoom";
-    private string RightTunnelRoom = "RightTunnel";
-    private string LeftTunnelRoom = "LeftTunnel";
+    public int maxTry = 3;
 
     public void CheckPassword()
     {
-        PasswordUserInput = UserInput.text;
-
-        if(PasswordUserInput != null)
+        if(UserInput.text == DoorPassword)
         {
-            if((UTRoom == SceneManager.GetActiveScene().name) && (PasswordUserInput == UnderTunnel))
-            {
-                SceneManager.LoadScene(correct);
-            }else if((FKRoom == SceneManager.GetActiveScene().name) && (PasswordUserInput == RefrigeratorRoom))
-            {
-                SceneManager.LoadScene(correct);
-            }else if((DRoom == SceneManager.GetActiveScene().name) && (PasswordUserInput == DiningRoom))
-            {
-                SceneManager.LoadScene(correct);
-            }else if((WorkRoom == SceneManager.GetActiveScene().name) && (PasswordUserInput == WorkspaceRoom))
-            {
-                SceneManager.LoadScene(correct);
-            }else if((WareRoom == SceneManager.GetActiveScene().name))
-            {
-                string PasswordUserInput2 = UserInput2.text;
-                if((PasswordUserInput == WarehouseRoom) && (PasswordUserInput2 == WarehouseRoom2))
-                {
-                    SceneManager.LoadScene(correct);
-                }else{
-                    SceneManager.LoadScene(failed);
-                }
-            }else if((RightTunnelRoom == SceneManager.GetActiveScene().name) && (PasswordUserInput == RightTunnel))
-            {
-                SceneManager.LoadScene(correct);
-            }else if((LeftTunnelRoom == SceneManager.GetActiveScene().name) && (PasswordUserInput == LeftTunnel))
-            {
-                SceneManager.LoadScene(correct);
-            }else{
-                SceneManager.LoadScene(failed);
-            }
+            CorrectPanel.SetActive(true);
+            Destroy(Door);
+        } else
+        {
+            IncorrectPanel.SetActive(true);
+            maxTry--;
+        }
+
+        if(maxTry < 0)
+        {
+            SceneManager.LoadScene("GameOver");
         }
     }
 }
