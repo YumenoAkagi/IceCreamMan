9e8b5119fb5d517f094534f7d3ea886a726516fc
YumenoAkagi <68376091+YumenoAkagi@users.noreply.github.com>
2021-12-25 19:30:43 +0700

    level 1 to 2 trigger

---
+++ Assets/Scripts/Levels/NextLevelTrigger.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class NextLevelTrigger : MonoBehaviour
+{
+    public Animator transition;
+    public float transitionTime = 1f;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.transform.CompareTag("Player"))
+        {
+            LoadNextLevel();
+        }
+    }
+
+    IEnumerator LoadLevel(int index)
+    {
+        transition.SetTrigger("Start");
+
+        yield return new WaitForSeconds(transitionTime);
+
+        SceneManager.LoadScene(index);
+    }
+
+    public void LoadNextLevel()
+    {
+        StartCoroutine(LoadLevel(SceneManager.GetActiveScene().buildIndex + 1));
+    }
+}
