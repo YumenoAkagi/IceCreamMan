64e2503ba273b1589536cf1601dfa6fb12864446
YumenoAkagi <68376091+YumenoAkagi@users.noreply.github.com>
2021-12-30 22:53:36 +0700

    gunshot sfx added

--- Assets/Scripts/Player/PlayerRangedCombat.cs
+++ Assets/Scripts/Player/PlayerRangedCombat.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections;
+using System;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -7,12 +8,33 @@ public class PlayerRangedCombat : MonoBehaviour
     public Transform firingPoint;
     public GameObject bulletPrefab;
 
+    AudioSource gunshotSFX;
+
     // Update is called once per frame
     void Update()
     {
         if (Input.GetButtonDown("Fire2"))
         {
             Instantiate(bulletPrefab, firingPoint.position, firingPoint.rotation);
+            PlayGunShotSFX();
         }
+    }
+
+    void PlayGunShotSFX()
+    {
+        if(gunshotSFX == null)
+        {
+            if (FindObjectOfType<AudioManager>() == null)
+                return;
+
+            var audio = Array.Find(FindObjectOfType<AudioManager>().SFXAudios, x => x.name == "Player - Ranged");
+
+            if (audio == null)
+                return;
+
+            gunshotSFX = audio;
+        }
+
+        gunshotSFX.Play();
     }
 }
