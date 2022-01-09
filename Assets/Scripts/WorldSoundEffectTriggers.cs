1c005b0d74940da899209eed984b46432f75ba9b
YumenoAkagi <68376091+YumenoAkagi@users.noreply.github.com>
2022-01-09 17:14:22 +0700

    level 3 door system

---
+++ Assets/Scripts/WorldSoundEffectTriggers.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WorldSoundEffectTriggers : MonoBehaviour
+{
+    public AudioSource worldSFX;
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            // play sound effect
+            worldSFX.Play();
+        }
+    }
+}
