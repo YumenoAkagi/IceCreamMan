84b1b1b4eec65bff1b04489a0ca4889c1471b129
YumenoAkagi <68376091+YumenoAkagi@users.noreply.github.com>
2022-01-11 16:30:28 +0700

    show controls layout to player

---
+++ Assets/Scripts/Levels/SimpleNextLogic.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SimpleNextLogic : MonoBehaviour
+{
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Return))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+    }
+}
