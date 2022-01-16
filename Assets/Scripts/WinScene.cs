180b2243e98acf7f9b49d9922df8017c3ee42bc0
YumenoAkagi <68376091+YumenoAkagi@users.noreply.github.com>
2022-01-16 17:07:52 +0700

    win scene

---
+++ Assets/Scripts/WinScene.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class WinScene : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Return))
+        {
+            SceneManager.LoadScene(0);
+        }
+    }
+}
