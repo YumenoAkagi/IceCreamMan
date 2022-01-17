161786f840c456ca837e74f30459d27713220774
YumenoAkagi <68376091+YumenoAkagi@users.noreply.github.com>
2022-01-17 21:38:55 +0700

    clue open/close function

---
+++ Assets/Scripts/HUD/CloseAllClues.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CloseAllClues : MonoBehaviour
+{
+    public GameObject[] clues;
+    public void CloseAllOpenClues()
+    {
+        foreach(var clue in clues)
+        {
+            if (clue.activeInHierarchy)
+                clue.SetActive(false);
+        }
+    }
+}
--- Assets/Scripts/HUD/OpenClueSystem.cs
+++ Assets/Scripts/HUD/OpenClueSystem.cs
@@ -6,13 +6,14 @@ public class OpenClueSystem : MonoBehaviour
 {
     bool openCluePanel = false;
     public GameObject cluePanel;
+    public CloseAllClues closeAllCluesSys;
 
     void Update()
     {
         if(Input.GetKeyDown(KeyCode.E))
         {
             openCluePanel = !openCluePanel;
-
+            closeAllCluesSys.CloseAllOpenClues();
             cluePanel.SetActive(openCluePanel);
         }
     }
