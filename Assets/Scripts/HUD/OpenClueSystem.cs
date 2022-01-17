aa7161500541b0c4b0de6b2915ef3b551acc750a
YumenoAkagi <68376091+YumenoAkagi@users.noreply.github.com>
2022-01-17 21:17:01 +0700

    clue HUD

---
+++ Assets/Scripts/HUD/OpenClueSystem.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenClueSystem : MonoBehaviour
+{
+    bool openCluePanel = false;
+    public GameObject cluePanel;
+
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.E))
+        {
+            openCluePanel = !openCluePanel;
+
+            cluePanel.SetActive(openCluePanel);
+        }
+    }
+}
