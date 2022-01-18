21e8cb9828b64623ad0d74c5d72615b1301e9c49
YumenoAkagi <68376091+YumenoAkagi@users.noreply.github.com>
2022-01-18 22:51:55 +0700

    recover clue on retry

--- Assets/Scripts/Player/Respawn.cs
+++ Assets/Scripts/Player/Respawn.cs
@@ -21,6 +21,23 @@ public class Respawn : MonoBehaviour
         player.SetActive(true);
         hud.SetActive(true);
 
+        if(SceneManager.GetActiveScene().name == "Level2")
+        {
+            // unlock all level 1 clue
+            var buttons = hud.GetComponentInChildren<CloseAllClues>(true).buttons;
+            for(int i=0; i < 2; i++)
+            {
+                buttons[i].SetActive(true);
+            }
+        } else if(SceneManager.GetActiveScene().name == "Level3Left")
+        {
+            var buttons = hud.GetComponentInChildren<CloseAllClues>(true).buttons;
+            for (int i = 0; i < 5; i++)
+            {
+                buttons[i].SetActive(true);
+            }
+        }
+
         PlayerPrefs.SetInt(RETRY_LEVEL_KEY, 0);
     }
 }
